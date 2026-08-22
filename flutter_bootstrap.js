56e2edf5f45b056ea91167c1e0f607ec05b7e949
Eslam Hossam <eslamhossam.tech@gmail.com>
2026-08-22 03:21:42 +0300

    add NextCart Project model

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "683685329"
+    serviceWorkerVersion: "745550445"
   }
 });
